6237e37504dd30735b7fe4167b1419181193a6d8
EmanueleTauro <emanuele.tauro@polimi.it>
2023-11-13 23:18:19 +0100

    Added Draw and Reset buttons. Changed background style. Working commit.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,7 @@
-import { spawn_snow, spawnSnowCSS} from "./snow.js";
-import {createTable} from "./tombola.js";
+import {spawn_snow, spawnSnowCSS} from "./snow.js";
+import {createTable, drawRandomNumber, highlightNumber, popupNumber} from "./tombola.js";
 
-
+let alreadyGenerated = [];
 let snowflakes_count = 300;
 
 window.onload = function () {
@@ -13,3 +13,29 @@ window.onload = function () {
     createTable(9, 10);
 
 }
+
+// Function to run when the button is clicked
+export function drawTombolaNumber() {
+    let number = drawRandomNumber(alreadyGenerated);
+    alreadyGenerated.push(number);
+    console.log (number);
+    popupNumber(number);
+    highlightNumber(number);
+}
+
+// Function to run when the reset button is clicked
+export function resetTombola() {
+    // Ask for confirmation
+    let confirmation = confirm("Sei sicuro di voler resettare la Tombola?");
+    if (!confirmation) {
+        console.log("Tombola non resettata")
+        console.log(alreadyGenerated)
+        return;
+    }
+    alreadyGenerated = [];
+    console.log("Tombola resettata");
+}
+
+// Add event listener to the button
+document.getElementById("startButton").addEventListener("click", drawTombolaNumber);
+document.getElementById("resetButton").addEventListener("click", resetTombola);
--- Scripts/tombola.js
+++ Scripts/tombola.js
@@ -1,5 +1,6 @@
-// Function to generate numbers from 1 to 90
+import {smorfia} from "./smorfia.js";
 
+// Function to generate numbers from 1 to 90
 function generateNumbers(rows, cols) {
     let numbers = [];
     for (let i = 1; i <= rows * cols; i++) {
@@ -21,4 +22,53 @@ export function createTable(rows, cols) {
             cell.textContent = numbers[i * cols + j];
         }
     }
+}
+
+// Function to generate random numbers for the tombola
+export function drawRandomNumber(alreadyGenerated) {
+    if (alreadyGenerated.length === 90) {
+        return "FINISHED";
+    }
+    let number = Math.floor(Math.random() * 90) + 1;
+    while (alreadyGenerated.includes(number)) {
+        number = Math.floor(Math.random() * 90) + 1;
+    }
+    return number
+}
+
+// Function that makes the number appear as red and bold in the table
+export function highlightNumber(number) {
+    let table = document.getElementById("numberTable");
+    let rows = table.rows;
+    let cols = rows[0].cells.length;
+
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+
+        for (let j = 0; j < cols; j++) {
+            let cell = row.cells[j];
+            if (cell.textContent === number.toString()) {
+                //cell.style.color = "#ff0000";
+                cell.style.backgroundColor = "#e99c37";
+                cell.style.fontWeight = "bold";
+                return;
+            }
+        }
+    }
+}
+
+// Function that makes a popup appear when a number is drawn
+export function popupNumber(number) {
+    let popup = document.getElementById("popup");
+    let popupText = document.getElementById("popupText");
+    let popupNumber = document.getElementById("popupNumber");
+
+    // Show the popup
+    popup.style.display = "block";
+    popupNumber.textContent = "Numero: " + number;
+    popupText.textContent = smorfia[number];
+    // Hide the popup after 3 seconds
+    setTimeout(function () {
+        popup.style.display = "none";
+    }, 3000);
 }
